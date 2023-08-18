5beb9528aabe77f461571e9ce6200611c2d11ff0
IlinaMukherjee <97727432+IlinaMukherjee@users.noreply.github.com>
2023-08-18 13:51:36 +0530

    Update 6contact.py

--- pages/6contact.py
+++ pages/6contact.py
@@ -27,10 +27,4 @@ cform = """
      <button type="submit">Send</button>
 </form>
 """
-
-left_column, right_column = st.columns(2)
-with left_column:
-    st.markdown(cform, unsafe_allow_html=True)
-with right_column:
-
-
+st.markdown(cform, unsafe_allow_html=True)
