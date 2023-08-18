9669f2eeccb61855d65777900146bff2769908dd
IlinaMukherjee <97727432+IlinaMukherjee@users.noreply.github.com>
2023-08-18 13:39:47 +0530

    Update 4discover.py

--- pages/4discover.py
+++ pages/4discover.py
@@ -1,17 +1,11 @@
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+
 
 
 st.page_title=("Discover | AcademEase")
 
-def load_lottie_url(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
 
-lottie_file2 = load_lottie_url("https://lottie.host/9f68ec0a-1376-4db2-b1da-4126b0840e56/hVgQ6km0CM.json")
 
 
 with st.container():
@@ -21,7 +15,7 @@ with st.container():
         st.write("In the wise words of ChatGPT,")
         st.caption("Embrace the day with a thirst for knowledge - seize the opportunity to learn something new and expand your horizons today. Every discovery is a step towards personal growth and a more enriched tomorrow.")
     with right_column:
-        st_lottie(lottie_file2, height=200, key="yellow")
+
 
 st.sidebar.success("Where would you like to go?")
 
