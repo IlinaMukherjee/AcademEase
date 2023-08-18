d784b70c833c95d9757b02e33a57a091e39ccffc
IlinaMukherjee <97727432+IlinaMukherjee@users.noreply.github.com>
2023-08-18 13:45:19 +0530

    Update 4discover.py

--- pages/4discover.py
+++ pages/4discover.py
@@ -9,15 +9,11 @@ st.page_title=("Discover | AcademEase")
 
 
 with st.container():
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.title("Discover")
-        st.write("In the wise words of ChatGPT,")
-        st.caption("Embrace the day with a thirst for knowledge - seize the opportunity to learn something new and expand your horizons today. Every discovery is a step towards personal growth and a more enriched tomorrow.")
-    with right_column:
+    st.title("Discover")
+    st.write("In the wise words of ChatGPT,")
+    st.caption("Embrace the day with a thirst for knowledge - seize the opportunity to learn something new and expand your horizons today. Every discovery is a step towards personal growth and a more enriched tomorrow.")
 
-
-    st.sidebar.success("Where would you like to go?")
+st.sidebar.success("Where would you like to go?")
 
 
 
