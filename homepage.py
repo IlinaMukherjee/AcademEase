b50b0628cdd1d668f1b9db2890dca1cc3d040e80
IlinaMukherjee <97727432+IlinaMukherjee@users.noreply.github.com>
2023-08-18 13:44:23 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -19,19 +19,16 @@ with st.container():
     st.write("Ever wanted to streamline your path to success? Look no further! Introducing our comprehensive website, your gateway to a world of opportunities. Get started on your journey to professional growth by exploring a diverse range of internships, thrilling competitions, and invaluable resources - all conveniently collated in one place. Unleash your potential today and pave the way for a brighter tomorrow with us!")
     st.write("##")
     st.divider()
-    left_column, right_column = st.columns(2)
-    with right_column:
-        
-    with left_column:
-        st.subheader("From internships to resources, we have it all!")
-        st.write("##")
-        st.write(
-            """
-            - Find opportunities in all fields!
-            - Information on competitions, regional, national and international
-            - Resources— extra reading, books and other free materials!
-            - Write in our student-run magazine!
-            """
+
+    st.subheader("From internships to resources, we have it all!")
+    st.write("##")
+    st.write(
+         """
+        - Find opportunities in all fields!
+        - Information on competitions, regional, national and international
+        - Resources— extra reading, books and other free materials!
+         - Write in our student-run magazine!
+         """
         )
 
 
